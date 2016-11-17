9be38202f8c4e0bd0fcd66c88402bea4160efdf0
vitor_simao <vitor_simao@SNRSL1ELBIET052>
2016-11-17 10:08:36 -0200

    Layout pronto

--- index.php
+++ index.php
@@ -20,15 +20,24 @@ and open the template in the editor.
                 <ul>
                     <li><a class="<?= ($_GET["pg"]=="home"?"active":"") ?>" href="?pg=home"><i class="fa fa-home"></i><br> Home</a></li>
                     <li><a class="<?= ($_GET["pg"]=="produtos"?"active":"") ?>" href="?pg=produtos"><i class="fa fa-dropbox"></i><br>Produtos</a></li>
-                    <li><a class="dropdown" class="<?= ($_GET["pg"]=="servicos"?"active":"") ?>" href="?pg=servicos"><i class="fa fa-wrench"></i><br> Serviços</a></li>
-                    <li><a class="<?= ($_GET["pg"]=="sobre"?"active":"") ?>" href="?pg=sobre"><i class="fa fa-users"></i><br>Sobre</a></li>
+                    <li class="dropdown"><a class="<?= ($_GET["pg"]=="servicos"||$_GET["pg"]=="faq"||$_GET["pg"]=="mapa"||$_GET["pg"]=="faq"||$_GET["pg"]=="descarte"?"active":"") ?>" href="?pg=servicos" class="dropbtn"><i class="fa fa-wrench"></i><br> Serviços</a>
+                        <ul>
+                            <div class="dropdown-content">
+                            <a class="<?= ($_GET["pg"]=="faq"?"active":"") ?>" href="?pg=faq">FAQ</a>
+                            <a class="<?= ($_GET["pg"]=="mapa"?"active":"") ?>" href="?pg=mapa">Mapa de riscos</a>
+                            <a class="<?= ($_GET["pg"]=="descarte"?"active":"") ?>" href="?pg=descarte">Programa de descarte</a>
+    </div>
+                        </ul>
+                    </li>
+                    <li><a class="<?= ($_GET["pg"]=="equipe"?"active":"") ?>" href="?pg=equipe"><i class="fa fa-users"></i><br>Equipe</a></li>
                     <li><a class="<?= ($_GET["pg"]=="contato"?"active":"") ?>" href="?pg=contato"><i class="fa fa-book"></i><br>Contato</a></li>
+                    <li style="float: right;"><a class="<?= ($_GET["pg"]=="sobre"?"active":"") ?>" href="?pg=sobre "><i class="fa fa-search"></i><br>Sobre</a></li>
                 </ul>
             </div>
             <div id="content">
-                <div>
+                <div id="aa">
                     <?php
-                    $pg = $_GET["pg"];
+                    @$pg = $_GET["pg"];
                     if (isset($pg)) {
                         include_once $pg . '.php';
                     }
