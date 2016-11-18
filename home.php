72764bd5bd4680e3f4623b767524e1fbc6ede973
eduardo_b_andrade <eduardo_b_andrade@SNRSL1ELBIET057>
2016-11-18 08:19:34 -0200

    fiz um eduardo.php

--- home.php
+++ home.php
@@ -11,30 +11,63 @@
             <img src="img/0101.jpg" width="300" height="200">
         </a>
         <div class="desc">
-            Celular Samsung Galaxy j5, Dual chip, 16Gb <br>
-            R$: 500,00
+            Samsung Galaxy j5 Dual chip 16Gb <br>
+            R$:500,00
         </div>
     </div>
 </div>
 <div class="promocao">
     <div class="img">
         <a href="#">
-            <img src="img/0101.jpg" width="300" height="200">
+            <img src="img/moto g.jpg" width="300" height="200">
         </a>
         <div class="desc">
-            Celular Samsung Galaxy j5, Dual chip, 16Gb <br>
-            R$: 500,00
+            Motorola Moto G 4 geração 32Gb <br>
+            R$:1.100,00
         </div>
     </div>
 </div>
 <div class="promocao">
     <div class="img">
         <a href="#">
-            <img src="img/0101.jpg" width="300" height="200">
+            <img src="img/x.jpg" width="300" height="200">
         </a>
         <div class="desc">
-            Celular Samsung Galaxy j5, Dual chip, 16Gb <br>
-            R$: 500,00
+           Motorola Moto X Play Dual Chip 16Gb <br>
+            R$:1.200,00
+        </div>
+    </div>
+</div>
+<div class="promocao">
+    <div class="img">
+        <a href="#">
+            <img src="img/6.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+           iPhone 6 16GB Prata iOS 8 <br>
+            R$:2.199,00
+        </div>
+    </div>
+</div>
+<div class="promocao">
+    <div class="img">
+        <a href="#">
+            <img src="img/zen.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+           Asus Zenfone 2 Dual Chip 32Gb <br>
+            R$:900,00
+        </div>
+    </div>
+</div>
+<div class="promocao">
+    <div class="img">
+        <a href="#">
+            <img src="img/deluxe.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+           Asus Zenfone Deluxe Dual Chip 128Gb <br>
+            R$:2.299,00
         </div>
     </div>
 </div>
