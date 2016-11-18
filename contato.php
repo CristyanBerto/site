19092d27c604fd94570070317f53d8179f3ca598
vitor_simao <vitor_simao@SNRSL1ELBIET052>
2016-11-18 11:46:08 -0200

    so pa ter certeza

--- contato.php
+++ contato.php
@@ -1,9 +1,4 @@
 <fieldset>
 <b>Email para contato:</b> pesadaoeletronicos@hotmail.com <br>
 <b>Endereço: </b> Estrada da madeira - 3000 - Barragem, Rio do Sul - SC, 89165-000 
-
-
-
-
-
 </fieldset>
